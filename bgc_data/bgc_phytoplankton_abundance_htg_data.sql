d5a7fa21c4f65357fd9e709e163d0fcb004acbd4
mhidas <marty.hidas@utas.edu.au>
2021-09-06 17:21:06 +1000

    WIP: add comments and biovolume for phyto raw & htg

--- bgc_data/bgc_phytoplankton_abundance_htg_data.sql
+++ bgc_data/bgc_phytoplankton_abundance_htg_data.sql
@@ -1,21 +1,32 @@
-CREATE MATERIALIZED VIEW bgc_phytoplankton_abundance_htg_data AS
+-- Materialized view for Phytoplankton Higher Taxonomic Groups (HTG) products
+-- To be served as 2 WFS layers by Geoserver using output format csv-with-metadata-header.
+-- Each layer will pick one of the josnb columns (abundance or biovolume), which will be converted
+-- into separate CSV columns on output.
+CREATE MATERIALIZED VIEW bgc_phytoplankton_htg_data AS
   WITH grouped AS (
+    -- sum up abundances & biovolumes for each trip/group,
+    -- filtering out groups that are not phytoplankton
     SELECT
       trip_code,
       taxon_group,
-      sum(cell_l) AS cell_l
+      sum(cell_l) AS cell_l,
+      sum(biovolume_um3l) AS biovolume_um3l
     FROM bgc_phyto_raw
     WHERE taxon_group NOT IN ('Other', 'Coccolithophore', 'Diatom', 'Protozoa')
     GROUP BY trip_code, taxon_group
   ), pivoted AS (
-    SELECT
+    -- group all taxon groups from a given trip into a single row,
+    -- aggregating abundances and biovolumes into separate jsonb columns
+      SELECT
       trip_code,
-      jsonb_object_agg(taxon_group, cell_l) AS jsonb_abundances
-    FROM grouped
-    GROUP BY trip_code
+      jsonb_object_agg(taxon_group, cell_l) AS abundances,
+      jsonb_object_agg(taxon_group, biovolume_um3l) AS biovolumes
+      FROM grouped
+      GROUP BY trip_code
   )
   SELECT
     m.*,
-    coalesce(p.jsonb_abundances, '{"Ciliate": 0}'::jsonb) AS jsonb_abundances
+    coalesce(p.abundances, '{"Ciliate": 0}'::jsonb) AS abundances,
+    coalesce(p.biovolumes, '{"Ciliate": 0}'::jsonb) AS biovolumes
   FROM bgc_phytoplankton_map m LEFT JOIN pivoted p USING (trip_code)
 ;
--- bgc_data/bgc_phytoplankton_abundance_raw_data.sql
+++ bgc_data/bgc_phytoplankton_abundance_raw_data.sql
@@ -1,14 +1,21 @@
-CREATE MATERIALIZED VIEW bgc_phytoplankton_abundance_raw_data AS
+-- Materialized view for Phytoplankton Raw products
+-- To be served as 2 WFS layers by Geoserver using output format csv-with-metadata-header.
+-- Each layer will pick one of the josnb columns (abundance or biovolume), which will be converted
+-- into separate CSV columns on output.
+CREATE MATERIALIZED VIEW bgc_phytoplankton_raw_data AS
   WITH pivoted AS (
+    -- group all taxa from a given trip into a single row,
+    -- aggregating abundances and biovolumes into separate jsonb columns
     SELECT
       trip_code,
-      jsonb_object_agg(taxon_name, cell_l) AS jsonb_abundances
+      jsonb_object_agg(taxon_name, cell_l) AS abundances,
+      jsonb_object_agg(taxon_name, biovolume_um3l) AS biovolumes
     FROM bgc_phyto_raw
     GROUP BY trip_code
   )
   SELECT
     m.*,
---    p.jsonb_abundances
-    coalesce(p.jsonb_abundances, '{"Cylindrotheca closterium": 0}'::jsonb) AS jsonb_abundances
+    coalesce(p.abundances, '{"Cylindrotheca closterium": 0}'::jsonb) AS abundances,
+    coalesce(p.biovolumes, '{"Cylindrotheca closterium": 0}'::jsonb) AS biovolumes
   FROM bgc_phytoplankton_map m LEFT JOIN pivoted p USING (trip_code)
 ;
