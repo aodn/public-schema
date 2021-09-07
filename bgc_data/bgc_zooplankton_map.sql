7191c57c021c77b877ca59eb4b962c022907ffdf
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2021-09-07 14:54:25 +1000

    add bgc_zooplankton_map.sql to create materialised view for WMS

---
+++ bgc_data/bgc_zooplankton_map.sql
@@ -0,0 +1,26 @@
+-- This view is the basis for the WMS layer (seen on step 2 on AODN Portal).
+-- It also provides the metadata columns for all the phytoplankton products.
+CREATE MATERIALIZED VIEW bgc_zooplankton_map AS
+  SELECT
+    projectname AS "Project",
+    stationname AS "StationName",
+    stationcode AS "StationCode",
+    latitude AS "Latitude",
+    longitude AS "Longitude",
+    trip_code AS "TripCode",
+  --  TODO: sampledatelocal AT TIME ZONE "UTC" AS "SampleTime_UTC",
+    sampledatelocal AS "SampleTime_local",
+    extract(year from sampledatelocal)::int AS "Year_local",
+    extract(month from sampledatelocal)::int AS "Month_local",
+    extract(day from sampledatelocal)::int AS "Day_local",
+    to_char(sampledatelocal, 'HH24:MI') AS "Time_local24hr",
+    zoopsampledepth_m AS "SampleDepth_m",
+  --  TODO: CTD derived params:
+  --  "CTDSST_degC",
+  --  "CTDChlaSurf_mgm3",
+  --  "CTDSalinity_psu",
+    trip_code,
+    st_geomfromtext('POINT(' || longitude::text || ' ' || latitude::text || ')', 4326) AS geom
+  FROM bgc_trip
+  WHERE sampletype LIKE '%Z%'
+;
