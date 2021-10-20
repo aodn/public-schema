9c90513571ea7bc2dd050ebc67a8a29e79497e1e
mhidas <marty.hidas@utas.edu.au>
2021-10-20 12:57:22 +1100

    include hard-coded changelog for zooplankton HTG

--- bgc_data/bgc_zooplankton_abundance_htg_data.sql
+++ bgc_data/bgc_zooplankton_abundance_htg_data.sql
@@ -11,11 +11,37 @@ WITH grouped AS (
     FROM bgc_zoop_raw
     WHERE taxon_group NOT IN ('Other')
     GROUP BY trip_code, taxon_group
+), changelog AS (
+    -- virtual changelog table for groups that were not counted before 2013-03-01
+    SELECT unnest(ARRAY ['Ciliophora', 'Foraminifera']) AS taxon_group,
+           '2013-03-01'::date AS startdate
+), default_abundances AS (
+    -- for taxon groups affected by the change in counting, set default abundance values
+    -- (NULL for 'not looked for', 0 for 'not found')
+    SELECT m.trip_code,
+           c.taxon_group,
+           CASE
+               WHEN m."SampleTime_local" < c.startdate THEN NULL
+               ELSE 0.
+           END AS zoop_abundance_m3
+    FROM bgc_zooplankton_map m CROSS JOIN changelog c
+), defaults_and_grouped AS (
+    -- stack together observations and default values
+    SELECT trip_code, taxon_group, zoop_abundance_m3  FROM default_abundances
+    UNION ALL
+    SELECT trip_code, taxon_group, zoop_abundance_m3  FROM grouped
+), regrouped AS (
+    -- create a single row per trip/group, making sure observed abundances override default values
+    SELECT trip_code,
+           taxon_group,
+           max(zoop_abundance_m3) AS zoop_abundance_m3
+    FROM defaults_and_grouped
+    GROUP BY trip_code, taxon_group
 ), pivoted AS (
     -- aggregate all taxon groups per trip into a single row
     SELECT trip_code,
            jsonb_object_agg(taxon_group, zoop_abundance_m3) AS abundances
-    FROM grouped
+    FROM regrouped
     GROUP BY trip_code
 )
 -- join on to metadata columns, include a row for every trip with zooplankton samples taken
