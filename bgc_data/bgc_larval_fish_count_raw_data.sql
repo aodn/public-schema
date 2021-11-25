47c57ebb288c851a56d7f367ca38548ac3e9c1ce
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2021-11-25 14:06:39 +1100

    Larval fish product generation code: * bgc_larval_fish_map * bgc_larval_fish_count_raw_data

---
+++ bgc_data/bgc_larval_fish_count_raw_data.sql
@@ -0,0 +1,18 @@
+-- Materialized view for larval fish count raw product
+-- To be served as a WFS layer by Geoserver using output format csv-with-metadata-header,
+-- which will convert the jsonb `abundances` column into separate CSV columns on output.
+CREATE MATERIALIZED VIEW bgc_larval_fish_count_raw AS
+WITH pivoted AS (
+   SELECT 
+      i_sample_id, 
+      trip_code,
+      jsonb_object_agg(scientificname, taxon_count) AS abundances
+   FROM bgc_lfish_countraw
+   GROUP BY trip_code, i_sample_id 
+)
+-- join on to metadata columns, include a row for every trip with larval fish samples taken
+   SELECT 
+      m.*,
+      p.abundances
+   FROM bgc_larval_fish_map m LEFT JOIN pivoted p USING (trip_code, i_sample_id)
+;
