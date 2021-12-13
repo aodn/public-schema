bab878f763bc2e130f318f385935293005c32346
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2021-12-13 14:17:38 +1100

    update bgc_larval_fish_map so picks temperature + salinity from CTD or from underway

--- bgc_data/bgc_larval_fish_map.sql
+++ bgc_data/bgc_larval_fish_map.sql
@@ -1,7 +1,6 @@
 -- This view is the basis for the WMS layer (seen on step 2 on AODN Portal).
 -- It also provides the metadata columns for the larval fish product.
 CREATE MATERIALIZED VIEW bgc_larval_fish_map AS
---  TODO: CTD salinity and temperature for NRS stations (where lfs.projectname like '%NRS%')
    SELECT 
       lfs.projectname AS "Project",
       lfs.stationname AS "StationName",
@@ -15,8 +14,8 @@ CREATE MATERIALIZED VIEW bgc_larval_fish_map AS
       extract(day from lfs.sampledatelocal)::int AS "Day_local",
       to_char(lfs.sampledatelocal, 'HH24:MI') AS "Time_local24hr",
       lfs.geardepth_m AS "SampleDepth_m",
-      lfs.temperature_c AS "Temperature_degC",
-      lfs.salinity_psu AS "Salinity_psu",
+      CASE WHEN lfs.projectname LIKE '%NRS%' THEN sv."CTDSST_degC" ELSE lfs.temperature_c END AS "Temperature_degC",
+      CASE WHEN lfs.projectname LIKE '%NRS%' THEN sv."CTDSalinity_psu" ELSE lfs.salinity_psu END AS "Salinity_psu",
       lfs.sampvol_m3 AS "Volume_m3", 
       lfs.vessel AS "Vessel", 
       lfs.towtype AS "TowType",
@@ -25,6 +24,6 @@ CREATE MATERIALIZED VIEW bgc_larval_fish_map AS
       lfs.qc_flag AS "QC_flag", 
       lfs.i_sample_id,
       st_geomfromtext('POINT(' || longitude::text || ' ' || latitude::text || ')', 4326) AS geom
-   FROM bgc_lfish_samples lfs
+   FROM bgc_lfish_samples lfs LEFT JOIN nrs_ctd_surface_values sv USING (trip_code)
 ;
 
