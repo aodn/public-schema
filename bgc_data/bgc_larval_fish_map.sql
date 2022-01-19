93dd2f950cc857ce44d03c01efb9c288246fa537
mhidas <marty.hidas@utas.edu.au>
2022-01-19 15:29:55 +1100

    Revert the last three commits zooplankton and larval fish will be separate collections, so a combined map is not needed
    This reverts commit eee52c8fbc1d1b576449977c7ceb87b67175dec5.

--- bgc_data/bgc_larval_fish_map.sql
+++ bgc_data/bgc_larval_fish_map.sql
@@ -1,5 +1,6 @@
--- This view provides the metadata columns for the larval fish product.
-CREATE VIEW bgc_larval_fish_map AS
+-- This view is the basis for the WMS layer (seen on step 2 on AODN Portal).
+-- It also provides the metadata columns for the larval fish product.
+CREATE MATERIALIZED VIEW bgc_larval_fish_map AS
    SELECT 
       lfs.projectname AS "Project",
       lfs.stationname AS "StationName",
