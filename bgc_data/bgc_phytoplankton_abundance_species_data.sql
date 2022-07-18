9c2e756ccbdae40f61216818293397cd26dccbfa
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2022-07-18 15:38:59 +1000

    update methods in bgc_phytoplankton_abundance_species_data

--- bgc_data/bgc_phytoplankton_abundance_species_data.sql
+++ bgc_data/bgc_phytoplankton_abundance_species_data.sql
@@ -7,6 +7,7 @@ WITH bgc_phyto_raw_species AS (
     -- concatenate genus and species to create a simplified taxon name
     -- (without comments about with flagellates, cilliates etc...)
     SELECT trip_code,
+           methods,
            genus || ' ' || species AS taxon_name,
            cell_l
     FROM bgc_phyto_raw
@@ -16,19 +17,20 @@ WITH bgc_phyto_raw_species AS (
           species NOT LIKE '%/%' AND
           species NOT LIKE '%complex%' AND
           species NOT LIKE '%type%' AND
-          species NOT LIKE '%cyst%' 
+          species NOT LIKE '%cyst%'
 ), grouped AS (
     -- join changelog on to raw data, group by trip, species and changelog details
     SELECT r.trip_code,
+           r.methods,
            taxon_name,
            c.startdate,
            taxon_name != c.parent_name AS species_changed,
            sum(r.cell_l) AS cell_l
     FROM bgc_phyto_raw_species r LEFT JOIN bgc_phyto_changelog c USING (taxon_name)
-    GROUP BY trip_code, taxon_name, startdate, species_changed
+    GROUP BY trip_code, r.methods, taxon_name, startdate, species_changed
 ), species_affected AS (
     -- identify species affected by a taxonomy change
-    SELECT DISTINCT taxon_name, startdate
+    SELECT DISTINCT taxon_name, methods, startdate
     FROM grouped
     WHERE species_changed
 
@@ -36,6 +38,7 @@ WITH bgc_phyto_raw_species AS (
     -- for species affected by taxonomy change, set default abundance values
     -- (NULL for 'not looked for', 0 for 'not found')
     SELECT m.trip_code,
+           s.methods,
            s.taxon_name,
            CASE
                WHEN m."SampleTime_UTC" < s.startdate THEN NULL
@@ -44,25 +47,28 @@ WITH bgc_phyto_raw_species AS (
     FROM bgc_phytoplankton_map m CROSS JOIN species_affected s
 ), defaults_and_grouped AS (
     -- stack together observations and default values
-    SELECT trip_code, taxon_name, cell_l  FROM default_abundances
+    SELECT trip_code, methods, taxon_name, cell_l  FROM default_abundances
     UNION ALL
-    SELECT trip_code, taxon_name, cell_l  FROM grouped
+    SELECT trip_code, methods, taxon_name, cell_l  FROM grouped
 ), regrouped AS (
     -- create a single row per trip/species, making sure observed abundances override default values
     SELECT trip_code,
+           methods,
            taxon_name,
            max(cell_l) AS cell_l
     FROM defaults_and_grouped
-    GROUP BY trip_code, taxon_name
+    GROUP BY trip_code, taxon_name, methods
 ), pivoted AS (
     -- aggregate all species per trip into a single row
     SELECT trip_code,
+           methods,
            jsonb_object_agg(taxon_name, cell_l) AS abundances
     FROM regrouped
-    GROUP BY trip_code
+    GROUP BY trip_code, methods
 )
 -- join on to metadata columns, include a row for every trip with phytoplankton samples taken
 SELECT m.*,
+       p.methods AS "Method",
        p.abundances
 FROM bgc_phytoplankton_map m LEFT JOIN pivoted p USING (trip_code)
 ;
