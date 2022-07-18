79ea3e26ab954556c8f5db4ee0122949d812571c
Leonardo Laiolo <leonardo.laiolo@utas.edu.au>
2022-07-18 15:31:04 +1000

    update map

--- bgc_data/bgc_phytoplankton_map.sql
+++ bgc_data/bgc_phytoplankton_map.sql
@@ -1,7 +1,7 @@
 -- This view is the basis for the WMS layer (seen on step 2 on AODN Portal).
 -- It also provides the metadata columns for all the phytoplankton products.
 CREATE MATERIALIZED VIEW bgc_phytoplankton_map AS
-  SELECT
+SELECT
     "Project",
     "StationName",
     "StationCode",
@@ -18,11 +18,9 @@ CREATE MATERIALIZED VIEW bgc_phytoplankton_map AS
     "CTDSST_degC",
     "CTDChlaSurf_mgm3",
     "CTDSalinity_psu",
-    p.methods AS "Method",
     trip_code,
     geom
   FROM bgc_trip_metadata t LEFT JOIN nrs_ctd_surface_values c USING (trip_code)
-                           LEFT JOIN bgc_phyto_raw p USING (trip_code)
   WHERE sampletype LIKE '%P%' AND
         trip_code IN (SELECT DISTINCT trip_code from bgc_phyto_raw)
 ;
